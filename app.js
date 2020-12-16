8a47973fc3c48bc492ac757fd50cdffb42bd27c9
majckson <morganjackson595@gmail.com>
2020-12-15 17:43:58 -0800

    added JS

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 console.log("Welcome");
 
+var myVariableName = "Welcome";
+console.log(myVariableName);
+
+myVariableName = "login";
+console.log(myVariableName);
+
+var promptQuestion = "Are you over 21?";
+var response = prompt(promptQuestion);
+console.log(response);
+
+var alertMessage;
+var color;
+
+if (response === "yes") {alertMessage = "Perfect! Welcome";
+color = #32a832
+} else if (response === "no") {alertMessage = "Go find your adult"
+color = #c92c2c;
+}
+
+
+alert(alertMessage);
+
+var element = document.getElementById
+('response-output') 
+
+element.innerText = alertMessage;
+element.style.color = color 
