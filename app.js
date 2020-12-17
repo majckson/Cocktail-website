ba774d1623ba86011f946251c145511e2e1b795e
majckson <morganjackson595@gmail.com>
2020-12-16 17:11:09 -0800

    worked on while loop and added second prompt

--- app.js
+++ app.js
@@ -6,37 +6,34 @@ console.log(myVariableName);
 myVariableName = "login";
 console.log(myVariableName);
 
-
-
-
-
-
 function myPrompt(newPrompt, accept, reject){
     var response = prompt(newPrompt);
     response = response.toLowerCase();
     console.log(response);
 
-    var alertMessage = "";
+    var alertMessage;
 
     if (response === "yes") {
         alertMessage = accept;
     } else  {
-        alertMessage = reject;
-
         while (response.toLowerCase() != "yes"){
+            alert(reject);
+            console.log(response);
             response = prompt(newPrompt);
         }
         alertMessage = accept;
+        alert(alertMessage);
     }
     return alertMessage;
 }
 
-var alertMessageFinal = myPrompt("Are you over 21?", "Welcome!", "GTFO")
+myPrompt("Are you over 21?", "Welcome!", "GTFO")
 
-alert(alertMessageFinal);
+//var element = document.getElementById('Top'); //would change banner
 
-var element = document.getElementById('Top');
-
-element.innerText = alertMessageFinal;
+//element.innerText = alertMessageFinal; //txt that poppulates banner
 //element.style.color = color;
 //var temp = myPrompt("Is Hazel a dork?", "I agree", "Try again")
+
+var temp2 = myPrompt("Are you ready for the Holidays?", "Cool Cool", "Ok, Grinch" )
+console.log(temp2);
